17f0e2429e80d2ad2ef1e020d6c1de09f2c4437b
adalbertoZauli <adalberto@zauli.med.br>
2018-06-11 11:12:04 -0300

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Adalberto dos Santos on 11/06/18.
+//  Copyright © 2018 Adalberto dos Santos. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
